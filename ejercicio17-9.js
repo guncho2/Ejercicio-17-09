19d362ee8d8431420c490b44a957dcb8a1c83a88
Franncogonza <gonzalez.francodavid@hotmail.com>
2019-09-17 22:11:40 -0300

    Ejercicio 17-9

---
+++ ejercicio17-9.js
@@ -0,0 +1,28 @@
+var Persona = function(edad, nombre) {
+    this.edad = edad;
+    this.nombre = nombre;
+
+}
+
+var empresa = function(edad) {
+    this.edadTope = edad;
+
+
+}
+
+
+var personasNoAptas = function() {
+
+    var persona1 = new Persona(24, "Flor");
+    var persona2 = new Persona(35, "Edu");
+    var persona3 = new Persona(35, "Franco");
+
+    var empresa1 = new empresa(60);
+
+    var recursos = [persona1, persona2, persona3];
+
+    var habilitados = recursos.filter(persona => persona.edad > empresa1.edadTope);
+    console.log(habilitados.length);
+
+
+}
